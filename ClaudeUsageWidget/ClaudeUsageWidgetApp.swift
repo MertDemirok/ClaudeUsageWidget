0633c5588be704cb77669c12268b9f202b130cfa
mertdemirok <mertdemiirok@gmail.com>
2026-07-09 14:00:27 +0300

    Initial Commit

---
+++ ClaudeUsageWidget/ClaudeUsageWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageWidgetApp.swift
+//  ClaudeUsageWidget
+//
+//  Created by Mert Demirok on 9.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
